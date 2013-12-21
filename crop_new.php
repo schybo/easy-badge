66500289936a5942243304525276fa1d8494913c
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2013-12-21 12:47:03 -0500

    Got nice ending page

--- crop_new.php
+++ crop_new.php
@@ -1,3 +1,42 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>EasyBadge Creator</title>
+  <meta name="description" content="EasyBadge Creator - create badges with fancy rims for gamification in the work place easily">
+  <meta name="author" content="SitePoint">
+  <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+  <link rel="stylesheet" href="/css/style.css" type="text/css" charset="utf-8" >
+  <style type="text/css">
+  .nothing{
+    font-family: 'vag_rounded_black_ssibold';
+  }
+  body{
+    font-family: 'cartogothic_stdregular', Helvetica,Arial,sans-serif;
+  }
+  </style>
+  <!-- Attach our CSS -->
+  <!--<link rel="stylesheet" href="/css/reveal.css">--> 
+  
+  <!-- Attach necessary scripts -->
+  <!-- <script type="text/javascript" src="jquery-1.4.4.min.js"></script> -->
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.min.js"></script>
+  <!--<script type="text/javascript" src="jquery.reveal.js"></script>-->
+</head>
+<body>
+<?php include_once("analyticstracking.php") ?>
+<div class="top">
+  <img src="/images/logo_bad.png">
+</div>
+<div class="first_other">
+  <div class="head1">Thank you for using EasyBadge</div>
+  <div class="head2">Please right click the image and choose 'Save As' to download them image</div>
+</body>
+</html>
+
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
@@ -44,9 +83,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     imagepng($image, 'badge.png');
     //header("Location: http://easybadge.herokuapp.com/badge.png");
     //exit();
-    echo '<img src="' . $save_file . '"/>';
-    /*echo '<div class="img_holder"><img src="' . $save_file . '"/></div></div><div class="badge_line"><img src="/badge_line.png"></div><div class="contributer"><p>Created by Brent Scheibelhut</p></div>
-      <div class="copyright"><p>Copyright &#169; Brent Scheibelhut</p></div>';*/
+    //echo '<img src="' . $save_file . '"/>';
+    echo '<div class="img_holder"><img src="' . $save_file . '"/></div></div><div class="badge_line"><img src="/badge_line.png"></div><div class="contributer"><p>Created by Brent Scheibelhut</p></div>
+      <div class="copyright"><p>Copyright &#169; Brent Scheibelhut</p></div>';
     imagedestroy($image);
 
     exit;
